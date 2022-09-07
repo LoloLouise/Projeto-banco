613644b1de1edd6cf5c8e88de62c9f12760bb106
Jonas Estevam <jonas_estevam@live.com>
2022-09-07 19:57:06 -0300

    added the contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,5 +1,58 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import PartnersImage from '../../assets/images/conversation.svg';
+import { BsFacebook, BsInstagram, BsTwitter } from 'react-icons/bs';
+import './styles.scss';
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className='contactWrapper'>
+      <div className='contactLeftContainer'>
+        <div className='contactTitleContainer'>
+          <div className='contactTitle'>
+            <span>Con</span>
+            <strong>tato</strong>
+          </div>
+          <p>
+            Mussum Ipsum, Suco de cevadiss deixa as pessoas mais interessantis.
+          </p>
+        </div>
+        <div className='iconsContainer'>
+          <Button variant='secondary'>
+            <BsFacebook />
+          </Button>
+
+          <Button variant='secondary'>
+            <BsInstagram />
+          </Button>
+
+          <Button variant='secondary'>
+            <BsTwitter />
+          </Button>
+        </div>
+
+        <Form className='contactForm'>
+          <Form.Group className='mb-3'>
+            <Form.Label>Seu e-mail</Form.Label>
+            <Form.Control type='email' placeholder='E-mail' />
+          </Form.Group>
+
+          <Form.Group className='mb-3'>
+            <Form.Label>Sua mensagem</Form.Label>
+            <Form.Control as='textarea' placeholder='Mensagem' />
+          </Form.Group>
+          <Button variant='secondary' type='submit'>
+            Enviar
+          </Button>
+        </Form>
+      </div>
+      <div className='contactRightContainer'>
+        <div className='contactImage'>
+          <img src={PartnersImage} alt='' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
